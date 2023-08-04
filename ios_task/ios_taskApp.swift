d14805addc5fa08f687de1fb577b1759ec69a8f9
Imran <imran40031@gmail.com>
2023-08-04 16:18:35 +0600

    Initial Commit

---
+++ ios_task/ios_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_taskApp.swift
+//  ios_task
+//
+//  Created by Md IMRAN on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
